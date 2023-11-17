edff081eefa2f426bf1e841b0f75b2ad858933ab
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-17 21:38:11 +0300

    chore: Add extra `nix.settings.substituters` for `hyprland`

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,7 +1,20 @@
+# INFO: Hyprland, the smooth Wayland compositor
+
 { inputs, ... }: {
-  imports = [
-    inputs.hyprland.homeManagerModules.default
-  ];
+  imports = [ inputs.hyprland.homeManagerModules.default ];
+
+  # Extra binary caches to avoid compiling from source
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
   };
