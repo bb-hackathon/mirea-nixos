11eb629538bba4b838c4ba1f8d1c12468488f684
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-17 10:13:12 +0300

    feat: Add minimal `hardware-configuration`

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -1,8 +1,39 @@
 { ... }: {
   fileSystems = {
     "/" = {
-      device = "/dev/sda1";
+      device = "/dev/vda2";
       fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+    "boot" = {
+      device = "/dev/vda1";
+      fsType = "vfat";
+    };
+  };
+
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [];
+    
+    initrd = {
+      availableKernelModules = [
+        "ahci"
+        "xhci_pci"
+        "virtio_pci"
+        "virtio_blk"
+      ];
+      kernelModules = [];
+    };
+
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/vda";
+        efiSupport = true;
+        # efiInstallAsRemovable = true;
+      };
+      efi.efiSysMountPoint = "/boot";
     };
   };
 
