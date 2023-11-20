c89a7753f201ba8be47e3e8ece111af876ca76ad
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-20 15:21:32 +0300

    fix: Add `hyprland.cachix.org` to `nix` extra caches

--- home/default.nix
+++ home/default.nix
@@ -8,13 +8,22 @@
       allowUnfreePredicate = (_: true);
     };
   };
-
+  
+  # Extra binary caches to avoid compiling from source
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings = {
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
     };
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 
   systemd.user.startServices = "sd-switch";
