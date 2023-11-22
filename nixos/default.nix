a2a487a31f70fcf983f4f76a83e83500b5cf5362
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-22 20:31:58 +0300

    fix(nix): Add more binary caches to avoid compiling stuff from source

--- nixos/default.nix
+++ nixos/default.nix
@@ -19,8 +19,21 @@
   nix = {
     settings = {
       trusted-users = [ "root" "@wheel" ];
-      # Flakes support
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+
+      # Extra binary caches
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
     };
   };
 
