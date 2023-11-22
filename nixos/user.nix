a3095b060f8f757dea9e03eb781c99f6b8db0426
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-22 20:23:36 +0300

    feat(nixos/user): Set `user`'s `shell` to `fish`

--- nixos/user.nix
+++ nixos/user.nix
@@ -1,10 +1,27 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
   users.users = {
     "user" = {
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
       packages = with pkgs; [ home-manager ];
+      shell = pkgs.fish; # See below!
+      extraGroups = [
+        "wheel"
+        "video"
+        "audio"
+        "input"
+      ] ++ ifTheyExist [
+        "networkmanager"
+        "docker"
+        "git"
+        "libvirtd"
+      ];
     };
   };
+
   home-manager.users."user" = import ../home;
+
+  # A friendly interactive shell (an alternative to bash)
+  programs.fish.enable = true;
 }
