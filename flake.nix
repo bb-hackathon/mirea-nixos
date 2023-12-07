020d1253b17645d6f048e8f33b146ca0c2f042e5
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-12-07 20:48:42 +0300

    chore(deps): Add `hyprland-plugins` input

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
 
     # Modules
     hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
   outputs = {
